0cf9ac08a2eacbcb101a0b58dcf1a1afe9da75f2
EnzoRobaina <enzo.robaina@gmail.com>
2019-04-08 13:38:03 -0300

    Finaliza funcionalidade da api

--- api.php
+++ api.php
@@ -6,10 +6,13 @@
     date_default_timezone_set('America/Fortaleza');
     $date = date('d-m-Y h:i:s a', time());
     $curl = curl_init('https://academico.iff.edu.br/');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    
     $result = curl_exec($curl);
     $status = '';
-    $http_code = curl_getinfo ( $result, CURLINFO_HTTP_CODE );
+    $http_code = curl_getinfo($curl,CURLINFO_HTTP_CODE);
     curl_close($curl);
+
     if($http_code == 200){
         $status = "Online";
     }
@@ -18,9 +21,10 @@
     }
 
 
-    $data =array(
+    $data = array(
         "response" => $date.", o Acadêmico está ".$status
     );
     
-    echo json_encode($data, JSON_UNESCAPED_UNICODE);
+    $response = json_encode($data, JSON_UNESCAPED_UNICODE);
+    echo $response;
 ?>
